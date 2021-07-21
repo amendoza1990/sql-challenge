026447c88e1f28849f8b72728d744bba152b8c5c
Alejandra Mendoza <amendozads@gmail.com>
2021-07-21 02:46:56 -0700

    Analysis 7

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -123,10 +123,17 @@ LEFT JOIN departments d
 ON de.dept_no = d.dept_no
 WHERE dept_name = 'Sales';
 
+--7.List all employees in the Sales and Development departments, including their 
+--  employee number, last name, first name, and department name.
 
-	
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+LEFT JOIN dept_emp de
+ON e.emp_no = de.emp_no
+LEFT JOIN departments d
+ON de.dept_no = d.dept_no
+WHERE dept_name IN ('Sales','Development');
+   
 
---7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
-
---8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+--8.In descending order, list the frequency count of employee last names, i.e., 
+--  how many employees share each last name.
