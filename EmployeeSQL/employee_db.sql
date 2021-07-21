e9ce03d9e22599797ff45939ca27f77456a5bbcd
Alejandra Mendoza <amendozads@gmail.com>
2021-07-21 01:50:03 -0700

    Analysis 3

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -87,19 +87,25 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%/1986';
 
---List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+--3.List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM dept_manager
+LEFT JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+LEFT JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
 
 
-List the department of each employee with the following information: employee number, last name, first name, and department name.
+--4.List the department of each employee with the following information: employee number, last name, first name, and department name.
 
 
-List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+--5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 
-List all employees in the Sales department, including their employee number, last name, first name, and department name.
+--6.List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
 
-List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+--7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 
-In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+--8.In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
