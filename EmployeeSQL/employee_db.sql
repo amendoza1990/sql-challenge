00cf2e8731d37609960a742b4b4d453d0c05c794
Alejandra Mendoza <amendozads@gmail.com>
2021-07-21 02:40:18 -0700

    Analysis 6

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -106,11 +106,25 @@ ON e.emp_no = de.emp_no
 LEFT JOIN departments d
 ON de.dept_no = d.dept_no;
 
---5.List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+--5.List first name, last name, and sex for employees 
+--  whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--6.List all employees in the Sales department, including their 
+--  employee number, last name, first name, and department name.
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+LEFT JOIN dept_emp de
+ON e.emp_no = de.emp_no
+LEFT JOIN departments d
+ON de.dept_no = d.dept_no
+WHERE dept_name = 'Sales';
 
 
---6.List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+	
 
 --7.List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
