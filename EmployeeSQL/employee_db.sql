f4147eb0e87db68712daa42263fa717b1c48bc8d
Alejandra Mendoza <amendozads@gmail.com>
2021-07-21 01:11:40 -0700

    Analysis 1

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -3,6 +3,8 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
+--Data Engineering
+
 CREATE TABLE "employees" (
     "emp_no" INTEGER   NOT NULL,
     "emp_title_id" VARCHAR   NOT NULL,
@@ -72,3 +74,33 @@ SELECT * FROM employees
 SELECT * FROM salaries
 SELECT * FROM titles
 
+--Data Analysis
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries
+ON salaries.emp_no = employees.emp_no;
+
+
+
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
